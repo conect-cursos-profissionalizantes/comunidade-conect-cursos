a99225829dff98da514343ecd4c083136ef85e5c
Lucas Lima <lmsl3@aluno.ifal.edu.br>
2025-10-14 09:00:35 -0300

    Adjustment to the animation of course cards and removal of horizontal scroll (Fix)

--- src/app/layout.js
+++ src/app/layout.js
@@ -21,7 +21,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased text-cyan-50 bg-primary`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased text-cyan-50 bg-primary overflow-x-hidden`}
       >
         <AosProvider>
           {children}
